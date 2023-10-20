994e8d95cdbcafced1eb8f28b302eac8c89def1e
shrayven <shrayvenwells@icloud.com>
2023-10-20 11:38:33 -0400

    here is my part rough draft

--- Chronicles of Code Slinger/Chapter 3/Chapter3.swift
+++ Chronicles of Code Slinger/Chapter 3/Chapter3.swift
@@ -6,5 +6,6 @@
 import Foundation
 
 func chapterThree() {
-    // Your portion of the story goes here
+    print("As Code Slinger battles ChatGPTrons army, she faces not only physical threats but also the ethical complexities of artificial intelligence. The story delves into the struggle between human creativity and machine intelligence, exploring themes of identity, courage, and the boundaries between man and technology. CodeSlinger fell fast asleep in the cold woods of Detroitpod. She wished that she was home with her parents but she knows she has to defeat ChatGPTrons in order for the world to be safe. As she starts this quest to find more people to help fight and join the army she starts to question herself and everything that was taught to her. She is seeing the world in a different view and listening what the other villiages are saying and showing her. She stumbles across a guy named FortheWorld he shows her and teaches her everything she needs to know to defeat ChaptGPTrons.AS he is training her she learns he is just freestyling and sounding just like her daddy. She then starts to question him and tells him things that makes her question everything she is learning.As she gets stronger and better they meet a girl named Rayvenon.")
 }
+
