31db4239bbc58bb6eeeb79abae7c175c35b7f8a2
shrayven <shrayvenwells@icloud.com>
2023-10-27 10:19:24 -0400

    here is mine

--- Chronicles of Code Slinger/Chapter 3/Chapter3.swift
+++ Chronicles of Code Slinger/Chapter 3/Chapter3.swift
@@ -5,7 +5,48 @@
 
 import Foundation
 
-func chapterThree() {
-    print("As Code Slinger battles ChatGPTrons army, she faces not only physical threats but also the ethical complexities of artificial intelligence. The story delves into the struggle between human creativity and machine intelligence, exploring themes of identity, courage, and the boundaries between man and technology. CodeSlinger fell fast asleep in the cold woods of Detroitpod. She wished that she was home with her parents but she knows she has to defeat ChatGPTrons in order for the world to be safe. As she starts this quest to find more people to help fight and join the army she starts to question herself and everything that was taught to her. She is seeing the world in a different view and listening what the other villiages are saying and showing her. She stumbles across a guy named FortheWorld he shows her and teaches her everything she needs to know to defeat ChaptGPTrons.AS he is training her she learns he is just freestyling and sounding just like her daddy. She then starts to question him and tells him things that makes her question everything she is learning.As she gets stronger and better they meet a girl named Rayvenon.")
+let greeting = "Wake up and lets get to work!"
+let girlName = "Lilly"
+let warriorName = "ChatGPTron"
+let armyName = "ChatGPTronArmy"
+let guyName = "Teacher Steven"
+var power = 1
+
+
+
+
+func powerBall() {
+    power += 1
+    print (" She now has power \(power)")
+    if power == 1 {
+        print("she has Fire")
+        }
+        if power == 2 {
+            print(" she has Water")
+        }
+    if power == 3 {
+        print(" she has Air")
+    }
+    if power == 4 {
+        print(" she has Earth")
+    }
 }
+
+
+
+
+
+
+
+func chapterThree() {
+    print( "The door opens and \(guyName) walks in and shouts \(greeting)  for \(girlName) to wake up")
+    print(" She has power \(power)")
+       power += 1
+    print(" She now has power \(power)")
+    powerBall()
+    powerBall()
+   
+    }
+
+
 
