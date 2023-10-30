00540da6cba8ea442bb055b751cfbd9b9ddf21f2
shrayven <shrayvenwells@icloud.com>
2023-10-30 09:09:19 -0400

    updated one

--- Chronicles of Code Slinger/Chapter 3/Chapter3.swift
+++ Chronicles of Code Slinger/Chapter 3/Chapter3.swift
@@ -38,13 +38,17 @@ func powerBall() {
 
 
 
-func chapterThree() {
+func chapterThree() { 
     print( "The door opens and \(guyName) walks in and shouts \(greeting)  for \(girlName) to wake up")
     print(" She has power \(power)")
        power += 1
     print(" She now has power \(power)")
     powerBall()
     powerBall()
+    print("\(girlName) is getting stronger and her powers are growing and she getting very confident in being able to help out in the war.")
+   print("\(guyName) lets \(girlName) know that evil is approaching and she needs to be ready for whatever and she needs to take it more seriously and stop eating every 2-3 mins.")
+    print(" \(girlName) has been using her \(power) powers in combos and learning how to move objects with them.")
+print("News just came in that \(warriorName) has awaken his \(armyName) and they are on the move headed towards all the towns.")
    
     }
 
